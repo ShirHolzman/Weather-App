dc1baeb81625c6bfa4854ea559f4f800ec94d3b7
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:20:48 +0200

    Update Rain.js

--- src/Components/WeatherIcons/Rain.js
+++ src/Components/WeatherIcons/Rain.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./Rain.css";
 
 const Rain = () => {
@@ -44,50 +43,3 @@ const Rain = () => {
 };
 
 export default Rain;
-=======
-import "./Rain.css";
-
-const Rain = () => {
-  return (
-   
-<div className="container">
-  <div className="canvas">
-    <div className="clouds">
-      <div className="cloud">
-        <div className="cloud-copy"></div>
-        <div className="rain">
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-        </div>
-      </div>
-      <div className="cloud">
-        <div className="cloud-copy"></div>
-        <div className="rain">
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-          <div className="drop"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-  );
-};
-
-export default Rain;
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
