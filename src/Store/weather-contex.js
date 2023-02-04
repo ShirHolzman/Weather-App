ad5cb7085eff11e9627d8a76cc27437cb1e7712b
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:13:45 +0200

    Update weather-contex.js

--- src/Store/weather-contex.js
+++ src/Store/weather-contex.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 const WeatherContext = React.createContext({
@@ -8,15 +7,5 @@ const WeatherContext = React.createContext({
   timeZone: "Asia/Jerusalem"
 });
 
-=======
-import React from "react";
 
-const WeatherContext = React.createContext({
-  name: "Jerusalem,Israel",
-  latitude: 31.768319,
-  longtitude: 35.21371,
-  timeZone: "Asia/Jerusalem"
-});
-
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
 export default WeatherContext;
