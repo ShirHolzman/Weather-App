8e7cc711b841c51555f446c0e72785b642d59334
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:17:16 +0200

    Update Logo.js

--- src/Components/UI/Logo.js
+++ src/Components/UI/Logo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./Logo.css";
 import logo from "./logo.png";
 
@@ -11,17 +10,3 @@ const Logo = () => {
 };
 
 export default Logo;
-=======
-import "./Logo.css";
-import logo from "./logo.png";
-
-const Logo = () => {
-  return (
-    <div className="logo">
-      <img className="logo-img" src={logo} alt="logo" />
-    </div>
-  );
-};
-
-export default Logo;
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
