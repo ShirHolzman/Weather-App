55b88ecd84c739506c85286a96fca64beb527bd4
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:20:09 +0200

    Update Cloudy.js

--- src/Components/WeatherIcons/Cloudy.js
+++ src/Components/WeatherIcons/Cloudy.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./Cloudy.css";
 
 const Cloudy = () => {
@@ -14,21 +13,4 @@ const Cloudy = () => {
         </div>);
 };
 
-=======
-import "./Cloudy.css";
-
-const Cloudy = () => {
-    return(
-        <div className="container">
-          <div className="canvas">
-            <div className="clouds">
-              <div className="cloud"></div>
-              <div className="cloud"></div>
-              <div className="cloud"></div>
-            </div>
-          </div>
-        </div>);
-};
-
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
 export default Cloudy;
