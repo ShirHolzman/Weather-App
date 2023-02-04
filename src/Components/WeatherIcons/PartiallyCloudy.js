68b8a0003a72c63cc04cba8150f80ee6c60053f8
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:20:29 +0200

    Update PartiallyCloudy.js

--- src/Components/WeatherIcons/PartiallyCloudy.js
+++ src/Components/WeatherIcons/PartiallyCloudy.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./PartiallyCloudy.css";
 
 const PartiallyCloudy = () => {
@@ -22,28 +21,3 @@ const PartiallyCloudy = () => {
 };
 
 export default PartiallyCloudy;
-=======
-import "./PartiallyCloudy.css";
-
-const PartiallyCloudy = () => {
-  return (
-    <div className="container">
-      <div className="canvas">
-        <div className="sunny">
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-        </div>
-        <div className="cloud"></div>
-      </div>
-    </div>
-  );
-};
-
-export default PartiallyCloudy;
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
