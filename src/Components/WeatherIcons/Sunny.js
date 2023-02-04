b89b1c157ed950f2d47c2de1f3add55369f943a0
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:21:53 +0200

    Update Sunny.js

--- src/Components/WeatherIcons/Sunny.js
+++ src/Components/WeatherIcons/Sunny.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./Sunny.css";
 
 const Sunny = () => {
@@ -19,29 +18,3 @@ const Sunny = () => {
     </div>
   );
 };
-
-export default Sunny;
-=======
-import "./Sunny.css";
-
-const Sunny = () => {
-  return (
-    <div className="container">
-      <div className="canvas">
-        <div className="sunny">
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-          <div className="beam"> </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Sunny;
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
