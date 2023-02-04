eadd5c5e630a826e6ef15e81c472eebbd4a6d9dd
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:14:36 +0200

    Update AdditionalInfo.js

--- src/Components/Output/AdditionalInfo.js
+++ src/Components/Output/AdditionalInfo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Compass from "../UI/Compass";
 
 import React from "react";
@@ -40,46 +39,3 @@ const AdditionalInfo = (props) => {
 };
 
 export default AdditionalInfo;
-=======
-import Compass from "../UI/Compass";
-
-import React from "react";
-
-import "./AdditionalInfo.css";
-
-const AdditionalInfo = (props) => {
-  return (
-    <div className="additional-weather">
-      <p>Today's Highlights</p>
-      <div className="weather-container">
-        <div className="additional-info-card">
-          <h2>Wind Direction</h2>
-          <Compass className="compass" deg={props.data.windDirection} />
-        </div>
-        <div className="additional-info-card">
-          <h2>Wind Speed</h2>
-            <p>{props.data.windSpeed} km/h</p>
-        </div>
-        <div className="additional-info-card">
-          <h2>Radiation</h2>
-          <p>{props.data.radiation}</p>
-        </div>
-        <div className="additional-info-card">
-          <h2>Sunrise</h2>
-          <p>{props.data.sunrise.getHours() + ':' + props.data.sunrise.getMinutes()}</p>
-        </div>
-        <div className="additional-info-card">
-          <h2>Sunset</h2>
-          <p>{props.data.sunset.getHours() + ':' + props.data.sunset.getMinutes()}</p>
-        </div>
-        <div className="additional-info-card">
-          <h2>Visibility</h2>
-          <p>{props.data.visibility} m</p>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default AdditionalInfo;
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
