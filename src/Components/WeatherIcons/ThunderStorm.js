720bb134a284768e3bb002764a1330cbf058dd90
ShirHolzman <124397293+ShirHolzman@users.noreply.github.com>
2023-02-05 01:22:18 +0200

    Update ThunderStorm.js

--- src/Components/WeatherIcons/ThunderStorm.js
+++ src/Components/WeatherIcons/ThunderStorm.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./ThunderStorm.css";
 
 const ThunderStorm = () => {
@@ -24,33 +23,3 @@ const ThunderStorm = () => {
           </div>
         </div>);
 };
-
-=======
-import "./ThunderStorm.css";
-
-const ThunderStorm = () => {
-    return(
-        <div className="container">
-          <div className="canvas">
-            <div className="cloud">
-              <div className="cloud-copy"></div>
-              <div className="rain">
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="drop"></div>
-                <div className="lightning"></div>
-              </div>
-            </div>
-          </div>
-        </div>);
-};
-
->>>>>>> 750a379b369ac59f8cbee5e62b88eca6388f1624
-export default ThunderStorm;
